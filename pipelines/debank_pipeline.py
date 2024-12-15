8de4755f00655feae02c1f1b7215e439723cf459
tomfutago <tomfutago@gmail.com>
2024-12-15 15:12:40 +0000

    feat: add load_user_all_simple_protocol_list

--- pipelines/debank_pipeline.py
+++ pipelines/debank_pipeline.py
@@ -67,6 +67,42 @@ def load_protocols():
   load_info = pipeline.run(api_source)
   print(load_info)
 
+def load_user_all_simple_protocol_list(wallet: str):
+  api_source = rest_api_source({
+    "client": {
+      "base_url": "https://pro-openapi.debank.com/",
+      "headers": {
+        "accept": "application/json",
+        "AccessKey": dlt.secrets["sources.debank.access_key"]
+      },
+      "paginator": "single_page",
+    },
+    "resource_defaults": {
+      "write_disposition": "replace",
+    },
+    "resources": [
+      {
+        "name": "debank_wallet_protocol_balance",
+        "endpoint": {
+          "path": "/v1/user/all_simple_protocol_list",
+          "params": {
+            "id": wallet,
+          },
+        },
+      },
+    ],
+  })
+
+  pipeline = dlt.pipeline(
+    pipeline_name="debank_wallet_protocol_balance",
+    destination=dlt.destinations.duckdb(duckdb_destination),
+    dataset_name="main"
+  )
+
+  load_info = pipeline.run(api_source)
+  print(load_info)
+
 if __name__ == "__main__":
-  load_chains()
-  load_protocols()
+  #load_chains()
+  #load_protocols()
+  load_user_all_simple_protocol_list(wallet="0x4fdb601aebf2c6ad947d97a00b7eeaf71cc5bf93")
