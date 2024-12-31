7e3d6a9c6a023d3f7e8b686d14d47d030d250ad6
tomfutago <tomfutago@gmail.com>
2024-12-31 10:09:41 +0000

    feat: export md to parquet

--- archived/tests/export_db.py
+++ archived/tests/export_db.py
@@ -1,9 +1,12 @@
+import os
 import duckdb
+from dotenv import load_dotenv
 
-# Connect to your database
+"""
+# connect to local db
 con = duckdb.connect('./data/wallets.duckdb')
 
-# Export table to Parquet
+# export tables to parquet
 con.execute("COPY _dlt_loads TO './data/parquet/_dlt_loads.parquet' (FORMAT 'parquet')")
 con.execute("COPY _dlt_pipeline_state TO './data/parquet/_dlt_pipeline_state.parquet' (FORMAT 'parquet')")
 con.execute("COPY _dlt_version TO './data/parquet/_dlt_version.parquet' (FORMAT 'parquet')")
@@ -13,3 +16,11 @@ con.execute("COPY debank_chains TO './data/parquet/debank_chains.parquet' (FORMA
 con.execute("COPY debank_protocols TO './data/parquet/debank_protocols.parquet' (FORMAT 'parquet')")
 con.execute("COPY debank_wallet_protocol_balance TO './data/parquet/debank_wallet_protocol_balance.parquet' (FORMAT 'parquet')")
 con.execute("COPY plan_mapping TO './data/parquet/plan_mapping.parquet' (FORMAT 'parquet')")
+"""
+
+# connect to md db
+load_dotenv()
+md_token = os.getenv("MD_TOKEN")
+
+con = duckdb.connect(f"md:?motherduck_token={md_token}")
+con.execute("EXPORT DATABASE './data/parquet' (FORMAT PARQUET);")
