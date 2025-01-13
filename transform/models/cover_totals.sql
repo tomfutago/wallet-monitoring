aed2daba404f69450a82d93bc49fba9c065a5dc9
tomfutago <tomfutago@gmail.com>
2025-01-13 14:46:32 +0000

    feat: cover totals model

---
+++ transform/models/cover_totals.sql
@@ -0,0 +1,33 @@
+model (
+  name prod.cover_totals,
+  kind view
+);
+
+with cover_exposed_agg as (
+  select
+    cover_id,
+    listing,
+    sum(usd_exposed) as usd_exposed,
+    sum(eth_exposed) as eth_exposed
+  from wallets.prod.cover_wallet_enriched
+  where is_active
+  group by 1, 2
+  having sum(usd_exposed) >= 0.01
+)
+
+select
+  c.cover_id::bigint as cover_id,
+  c.listing::varchar as listing,
+  c.is_plan::boolean as is_plan,
+  c.cnt_wallet::int as cnt_wallet,
+  c.usd_cover::double as usd_cover,
+  c.eth_cover::double as eth_cover,
+  cw.usd_exposed::double as usd_exposed,
+  cw.eth_exposed::double as eth_exposed,
+  (c.usd_cover / cw.usd_exposed)::double as coverage_ratio,
+  (c.usd_cover * 0.05)::double as usd_deductible,
+  (c.eth_cover * 0.05)::double as eth_deductible,
+  c.cover_start_date::date as cover_start_date,
+  c.cover_end_date::date as cover_end_date
+from md_wallets.cover_agg c
+  left join md_wallets.int_cover_wallet_agg cw on c.cover_id = cw.cover_id;
