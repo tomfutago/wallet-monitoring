821ee2e7ac0ffbb977b64a44d1a6430e6700b2de
tomfutago <tomfutago@gmail.com>
2025-01-13 13:35:48 +0000

    feat: listing protocol totals model

---
+++ transform/models/listing_protocol_totals.sql
@@ -0,0 +1,31 @@
+model (
+  name prod.listing_protocol_totals,
+  kind view
+);
+
+with listing_exposed_agg as (
+  select
+    product_id,
+    listing,
+    protocol,
+    sum(usd_exposed) as usd_exposed,
+    sum(eth_exposed) as eth_exposed
+  from wallets.prod.cover_wallet_enriched
+  where is_active
+  group by 1, 2, 3
+  having sum(usd_exposed) >= 0.01
+)
+
+select
+  pc.product_id::int as product_id,
+  pc.listing::varchar as listing,
+  pw.protocol::varchar as protocol,
+  pc.is_plan::boolean as is_plan,
+  pc.cnt_cover::int as cnt_cover,
+  pc.cnt_wallet::int as cnt_wallet,
+  pc.usd_cover::double as usd_cover,
+  pc.eth_cover::double as eth_cover,
+  pw.usd_exposed::double as usd_exposed,
+  pw.eth_exposed::double as eth_exposed
+from wallets.prod.listing_agg pc
+  left join listing_exposed_agg pw on pc.product_id = pw.product_id;
