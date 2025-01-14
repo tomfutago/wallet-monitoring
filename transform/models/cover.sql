ed3617bacda1d9a7a57226295d4d34ccddb261bb
tomfutago <tomfutago@gmail.com>
2025-01-14 13:58:04 +0000

    refactor: add bundled listing name and cover owner short, remove plan_id

--- transform/models/cover.sql
+++ transform/models/cover.sql
@@ -16,13 +16,11 @@ select distinct
   product_id::int as product_id,
   product_name::varchar as product_name,
   product_type::varchar as product_type,
-  case plan
-    when 'Entry Cover' then 1
-    when 'Essential Cover' then 2
-    when 'Elite Cover' then 3
-  end::int as plan_id,
   nullif(plan, '<nil>')::varchar as plan,
-  coalesce(nullif(plan, '<nil>'), product_name)::varchar as listing,
+  coalesce(
+    nullif(plan, '<nil>'),
+    concat(if(product_type = 'Bundled Protocol Cover', 'Bundled - ', ''), product_name)
+  )::varchar as listing,
   if(nullif(plan, '<nil>') is not null, true, false)::boolean as is_plan,
   cover_start_date::date as cover_start_date,
   cover_end_date::date as cover_end_date,
@@ -31,5 +29,6 @@ select distinct
   native_cover_amount::double as native_cover_amount,
   usd_cover_amount::double as usd_cover_amount,
   eth_cover_amount::double as eth_cover_amount,
-  cover_owner::varchar as cover_owner
+  cover_owner::varchar as cover_owner,
+  concat(left(cover_owner, 6), '..', right(cover_owner, 4))::varchar as cover_owner_short
 from wallets.main.cover_wallets;
