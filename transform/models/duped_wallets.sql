d91f7ca8651295d12ff91ceaa53c550272290cc7
tomfutago <tomfutago@gmail.com>
2025-01-14 09:46:25 +0000

    fix: exclude partners

--- transform/models/duped_wallets.sql
+++ transform/models/duped_wallets.sql
@@ -10,7 +10,15 @@ select
   count(*)::int as count
 from wallets.prod.cover_wallet
 where is_active
-  and wallet <> '0x40329f3e27dd3fe228799b4a665f6f104c2ab6b4' -- OpenCover
+  and wallet not in (
+    '0x40329f3e27dd3fe228799b4a665f6f104c2ab6b4', -- OpenCover
+    '0xe4994082a0e7f38b565e6c5f4afd608de5eddfbb', -- OpenCover
+    '0x5f2b6e70aa6a217e9ecd1ed7d0f8f38ce9a348a2', -- OpenCover
+    '0x8b86cf2684a3af9dd34defc62a18a96deadc40ff', -- TRM
+    '0x666b8ebfbf4d5f0ce56962a25635cff563f13161', -- Sherlock
+    '0x5b453a19845e7492ee3a0df4ef085d4c75e5752b', -- Liquid Collective
+    '0x2557fe0959934f3814c6ee72ab46e6687b81b8ca'  -- Ensuro
+  )
   and wallet in (
     select wallet from wallets.prod.cover_wallet group by 1 having count(*) > 1
   )
