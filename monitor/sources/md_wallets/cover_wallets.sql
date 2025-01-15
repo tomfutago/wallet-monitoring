21fb36dbfaad95eea32a545f95e20a99b9b43d2b
tomfutago <tomfutago@gmail.com>
2025-01-15 11:07:52 +0000

    fix: clean up updated source

--- monitor/sources/md_wallets/cover_wallets.sql
+++ monitor/sources/md_wallets/cover_wallets.sql
@@ -1,20 +1,8 @@
 select
   cover_id,
-  product_id,
-  product_name,
-  product_type,
-  plan,
-  listing,
-  is_plan,
-  cover_start_date,
-  cover_end_date,
-  is_active,
-  cover_asset,
-  native_cover_amount,
-  usd_cover_amount,
-  eth_cover_amount,
-  cover_owner,
-  wallet
+  wallet,
+  wallet_short,
+  is_active
 from prod.cover_wallet
 where is_active
 order by cover_id, wallet
