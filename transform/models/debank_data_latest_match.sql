403f2778289989e5475263e3a11db4e4c83ce565
tomfutago <tomfutago@gmail.com>
2025-01-16 15:29:41 +0000

    refactor: debank data latest views based on daily incremental

--- transform/models/debank_data_latest_match.sql
+++ transform/models/debank_data_latest_match.sql
@@ -3,23 +3,16 @@ model (
   kind view
 );
 
-with
-
-mapping_unique_procols as (
-  select distinct protocol, debank_name
-  from wallets.prod.listing_mapping
-)
-
 select
-  d.wallet::varchar as wallet,
-  m.protocol::varchar as protocol,
-  d.chain::varchar as chain,
-  d.net_usd_value::double as net_usd_value,
-  d.asset_usd_value::double as asset_usd_value,
-  d.debt_usd_value::double as debt_usd_value,
-  d.net_eth_value::double as net_eth_value,
-  d.asset_eth_value::double as asset_eth_value,
-  d.debt_eth_value::double as debt_eth_value,
-  d.load_ts::timestamp as load_ts
-from wallets.prod.debank_data_latest d
-  inner join mapping_unique_procols m on d.debank_name = m.debank_name;
+  load_dt::date as load_dt,
+  wallet::varchar as wallet,
+  protocol::varchar as protocol,
+  chain::varchar as chain,
+  net_usd_value::double as net_usd_value,
+  asset_usd_value::double as asset_usd_value,
+  debt_usd_value::double as debt_usd_value,
+  net_eth_value::double as net_eth_value,
+  asset_eth_value::double as asset_eth_value,
+  debt_eth_value::double as debt_eth_value
+from wallets.prod.debank_data_daily_match
+where is_latest;
