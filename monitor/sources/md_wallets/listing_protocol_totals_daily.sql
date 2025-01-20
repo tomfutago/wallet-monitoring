75d03e92865de11fcd77e6144588b3995ed81fce
tomfutago <tomfutago@gmail.com>
2025-01-20 17:04:30 +0000

    feat: new daily sources

---
+++ monitor/sources/md_wallets/listing_protocol_totals_daily.sql
@@ -0,0 +1,14 @@
+select
+  load_dt,
+  product_id,
+  listing,
+  protocol,
+  is_plan,
+  cnt_cover,
+  cnt_wallet,
+  usd_cover,
+  eth_cover,
+  usd_exposed,
+  eth_exposed
+from prod.listing_protocol_totals_daily
+order by 1, 2
