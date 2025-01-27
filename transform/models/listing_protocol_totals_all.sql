0b25d691ba8521d97115f0b9ac5697ac90e9d539
tomfutago <tomfutago@gmail.com>
2025-01-27 13:57:46 +0000

    feat: listing all aggs

---
+++ transform/models/listing_protocol_totals_all.sql
@@ -0,0 +1,23 @@
+model (
+  name prod.listing_protocol_totals_all,
+  kind view
+);
+
+with listing_protocol_totals_all_daily_ext as (
+  select *, row_number() over (partition by product_id, protocol order by load_dt desc) as load_dt_rn
+  from wallets.prod.listing_protocol_totals_all_daily
+)
+
+select
+  product_id::int as product_id,
+  listing::varchar as listing,
+  protocol::varchar as protocol,
+  is_plan::boolean as is_plan,
+  cnt_cover::int as cnt_cover,
+  cnt_wallet::int as cnt_wallet,
+  usd_cover::double as usd_cover,
+  eth_cover::double as eth_cover,
+  usd_exposed::double as usd_exposed,
+  eth_exposed::double as eth_exposed
+from listing_protocol_totals_all_daily_ext
+where load_dt_rn = 1;
